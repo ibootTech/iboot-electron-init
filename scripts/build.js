9769ad1bb5d7bc7c831cd7cf05b12e34ab9d3c8d
luohong1993 <luohong_studio@163.com>
2021-06-24 23:08:48 +0800

    update

--- scripts/build.js
+++ scripts/build.js
@@ -9,6 +9,7 @@ let electronProcess = null
 let manualRestart = false
 const buildConfig = {
   arch: 'x64',
+  platform: 'darwin',
   asar: true,
   dir: path.join(__dirname, '../dist'),
   ignore: /(^\/(src|node_modules|test|\.[a-z]+|README|yarn|static|dist\/web))|\.gitkeep/,
